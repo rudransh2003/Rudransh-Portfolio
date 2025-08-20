47ba8732f7b23bfae9975cb3306e8f21602e05ff
nikunjgupta02 <nikunjguptank00@gmail.com>
2025-08-20 18:30:56 +0530

    Final commit

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -1,11 +1,7 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { SiLeetcode } from "react-icons/si";
-
 export default function Hero() {
   return (
     <section id="home">
       <div className="flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-        {/* Left Section */}
         <div className="flex flex-col justify-center p-6 text-center lg:text-left">
           <div>
             <div className="mb-[.7rem]">
@@ -20,17 +16,14 @@ export default function Hero() {
             </div>
           </div>
 
-          {/* Call to Action */}
+          {/* Main Buttons */}
           <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-            {/* See My Work */}
             <a
               href="#work"
               className="px-8 py-3 text-lg font-semibold rounded border-2 border-[#FC4370] text-[#FC4370] hover:bg-[#FC4370] hover:text-white transition"
             >
               See my work
             </a>
-
-            {/* View Resume */}
             <a
               href="https://drive.google.com/file/d/1hX5OhzFIYgsO4GoD1fzuMpz3L4rsACXO/view?usp=sharing"
               target="_blank"
@@ -41,31 +34,31 @@ export default function Hero() {
             </a>
           </div>
 
-          {/* 🔗 Socials Section */}
-          <div className="flex flex-row items-center justify-center mt-6 space-x-6 lg:justify-start">
+          {/* Social Buttons */}
+          <div className="flex flex-row items-center justify-center mt-6 space-x-4 lg:justify-start">
             <a
               href="https://github.com/rudransh2003"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-300 hover:text-[#FC4370] transition text-3xl"
+              className="px-4 py-2 text-sm font-medium rounded border-2 border-[#FC4370] text-[#FC4370] hover:bg-[#FC4370] hover:text-white transition"
             >
-              <FaGithub />
+              GitHub
             </a>
             <a
               href="https://www.linkedin.com/in/rudransh-singh-chouhan-b46b8b26b/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-300 hover:text-[#FC4370] transition text-3xl"
+              className="px-4 py-2 text-sm font-medium rounded border-2 border-[#FC4370] text-[#FC4370] hover:bg-[#FC4370] hover:text-white transition"
             >
-              <FaLinkedin />
+              LinkedIn
             </a>
             <a
               href="https://leetcode.com/u/rudranshsingh_/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-300 hover:text-[#FC4370] transition text-3xl"
+              className="px-4 py-2 text-sm font-medium rounded border-2 border-[#FC4370] text-[#FC4370] hover:bg-[#FC4370] hover:text-white transition"
             >
-              <SiLeetcode />
+              LeetCode
             </a>
           </div>
         </div>
@@ -81,4 +74,4 @@ export default function Hero() {
       </div>
     </section>
   );
-} 
+}
